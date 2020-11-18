46198734a25a8072c8b533319c145a7840ad6b85
Frosty <59410234+Frosty8529@users.noreply.github.com>
2020-11-18 14:01:44 +0700

    Updated commands (#13)
    * Add files via upload
    
    * Delete commands.py
    
    * Reworked t!help and t!announce commands
    
    * A redundant comment over import removed
    
    * A redundant comment over import removed
    
    * A redundant comment over import removed

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ import random
 from discord.image.downloadImage import downloadImage
 from discord.compare.comparehis import comparehis
 
-
 from discord.count.count_func import call_count
 from discord.count.last_week import recent
 from discord.count.all_time import forever
@@ -24,46 +23,59 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = Bot(command_prefix='t!')
 bot.remove_command('help')
 
+
 @bot.command(name='server')
 async def fetchServerInfo(context):
-	guild = context.guild
-	await context.send(f'Server Name: {guild.name}')
-	await context.send(f'Server Size: {len(guild.members)}')
-	await context.send(f'Server Name: {guild.owner.display_name}')
+    guild = context.guild
+    await context.send(f'Server Name: {guild.name}')
+    await context.send(f'Server Size: {len(guild.members)}')
+    await context.send(f'Server Name: {guild.owner.display_name}')
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
 
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!')
 
+
 @bot.command(name='help')
 async def help(ctx):
-    embed = discord.Embed(title="Trace's available commands",
+    embed = discord.Embed(title="Trace's Available Commands",
                           description='',
                           colour=discord.Color.blue()
                           )
 
-    #embed.set_author(name=client.user.name, icon_url='https://discordpy.readthedocs.io/en/latest/_images/snake.png')
+    # embed.set_author(name=client.user.name, icon_url='https://discordpy.readthedocs.io/en/latest/_images/snake.png')
     embed.set_footer(text='Hope this helps!', icon_url='https://pbs.twimg.com/media/DcEu95UWAAIvoWP.png')
-    #embed.set_image(url='https://pbs.twimg.com/media/DcEu9_wW0AAE2VK.png')
+    # embed.set_image(url='https://pbs.twimg.com/media/DcEu9_wW0AAE2VK.png')
     embed.set_thumbnail(url='https://pbs.twimg.com/media/DcEu9_bXcAAQ-Cv.png')
 
-    embed.add_field(name="Find source  :face_with_raised_eyebrow: ", value="t!source <picture url>")
-    embed.add_field(name="Top 5 source found!  :pencil: ", value="t!top", inline=False)
-    embed.add_field(name="Add source! ", value="t!add <picture/video url>", inline=False)
+    embed.add_field(name="Find source  :face_with_raised_eyebrow: ", value="t!source <anime name> <picture url>")
+    embed.add_field(name="Top 5 source searched!  :lemon: ", value="t!top", inline=False)
+    embed.add_field(name="Top 5 source searched weekly!  :mango:  ", value="t!weekly", inline=False)
+    embed.add_field(name="Add source!  :open_mouth: ", value="t!add <picture/video url>", inline=False)
+    embed.add_field(name="Make an announcement  :pencil: ", value="t!announce #channel <message>", inline=False)
 
     await ctx.send(embed=embed)
 
-@bot.command(name='annouce')
-async def announce(ctx, channel: discord.TextChannel, *, msg):
-    await ctx.send('Success!')
-    await channel.send(f'{msg}')
 
-pic_ext = ['.jpg','.png','.jpeg']
+@bot.command(name='announce')
+async def announce(ctx, channel: discord.TextChannel, *, msg):
+    embed = discord.Embed(title="Announcement",
+                          description=f'{msg}',
+                          colour=discord.Color.dark_purple()
+                          )
+    await ctx.send('Success!')
+    await channel.send(embed=embed)
+
+
+pic_ext = ['.jpg', '.png', '.jpeg']
+
 
 @bot.command(name='add')
 async def add(ctx, name, url):
@@ -72,12 +84,13 @@ async def add(ctx, name, url):
     for ext in pic_ext:
         if url.endswith(ext):
             await ctx.send('picture')
-            await ctx.send(str(ctx.author.mention)+" \n"+ downloadImage(name, url))
+            await ctx.send(str(ctx.author.mention) + " \n" + downloadImage(name, url))
             run = True
             break
-    
-    if not run :
+
+    if not run:
         await ctx.send('command error')
+
 
 @bot.command(name='source')
 async def source(ctx, url):
@@ -88,30 +101,31 @@ async def source(ctx, url):
             source_path = comparehis(url)
             print(source_path)
             d, _ = os.path.split(source_path)
-            d, _= os.path.split(d)
-            _,name= os.path.split(d)
+            d, _ = os.path.split(d)
+            _, name = os.path.split(d)
             call_count(name)
-            await ctx.send(str(ctx.author.mention)+" \n"+ name)
-            info = json.load(open(d+'/metadata.json', 'r'))
-            file = discord.File(source_path, filename= "image"+"."+source_path.split(".")[-1])
+            await ctx.send(str(ctx.author.mention) + " \n" + name)
+            info = json.load(open(d + '/meta.json', 'r'))
+            file = discord.File(source_path, filename="image" + "." + source_path.split(".")[-1])
             print(source_path.split(".")[-1])
             embed = discord.Embed(
-                title=info["QUERY_FLAG_ROMANJI_NAME"]+"\n"+info["QUERY_FLAG_ENGLISH_NAME"],
+                title=info["QUERY_FLAG_ROMANJI_NAME"] + "\n" + info["QUERY_FLAG_ENGLISH_NAME"],
                 description=info["QUERY_FLAG_YEAR"],
                 colour=discord.Color.blue()
             )
             embed.add_field(name="Episode", value=info["QUERY_FLAG_EPISODES"], inline=False)
             embed.add_field(name="Ratings", value=info["QUERY_FLAG_RATINGS"], inline=False)
-            embed.add_field(name="Tags", value=info["QUERY_FLAG_TAG_NAME_LIST"].replace(",",", "),inline=False)
-            embed.add_field(name="\u200B", value= '\u200B')
-            embed.add_field(name="Best matched", value= '\u200B', inline=False)
-            embed.set_image(url="attachment://"+"image"+"."+source_path.split(".")[-1])
-            await ctx.send(file=file,embed=embed)
+            embed.add_field(name="Tags", value=info["QUERY_FLAG_TAG_NAME_LIST"].replace(",", ", "), inline=False)
+            embed.add_field(name="\u200B", value='\u200B')
+            embed.add_field(name="Best matched", value='\u200B', inline=False)
+            embed.set_image(url="attachment://" + "image" + "." + source_path.split(".")[-1])
+            await ctx.send(file=file, embed=embed)
             run = True
             break
 
-    if not run :
+    if not run:
         await ctx.send('command error')
+
 
 @bot.command(name='top')
 async def top(ctx):
@@ -175,7 +189,7 @@ async def weekly(ctx):
 
 
 @bot.command(name="rec")
-async def rec(ctx,tag1,tag2):
+async def rec(ctx, tag1, tag2):
     temp = []
     tag_list = json.load(open('../Trace/discord/data/label.json', 'r'))
     for title in tag_list.keys():
@@ -183,9 +197,6 @@ async def rec(ctx,tag1,tag2):
             temp.append(title)
 
     await ctx.send(random.choice(temp))
-
-
-
 
 
 bot.run(TOKEN)
